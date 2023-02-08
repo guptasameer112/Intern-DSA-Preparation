7ff2e06adad51715f365440e82e09a4c81a54f14
Sameer Gupta <37142500+guptasameer112@users.noreply.github.com>
2023-02-09 00:57:40 +0530

    Helpful with Practice

---
+++ Template.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+# include <bits/stdc++.h>
+# include <vector>
+# include <set>
+# define INF 1e9
+# define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+# define fr(a, b) for(int i = a; i < b; i++)
+# define sort(arr) sort(arr.begin(), arr.end())
+typedef long long ll;
+const int mod =  1e9 + 7;
+using namespace std;
+
+int max(int a, int b) { return (a > b) ? a : b; }
+
+int main()
+{
+    fast_io;
+    ll t, n, m, x, i, j, k;
+
+    // cin >> t;
+    // t = 1;
+
+    while (t--)
+    {
+        // code here
+        
+    }
+
+    return 0;
+}
