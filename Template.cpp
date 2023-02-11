cd8f93fb6b2072d2871e1b32af1fc786fc9cc42b
Sameer Gupta <37142500+guptasameer112@users.noreply.github.com>
2023-02-11 20:59:46 +0530

    added file reading

--- Template.cpp
+++ Template.cpp
@@ -5,17 +5,37 @@
 # define INF 1e9
 # define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 # define fr(a, b, i) for(int i = a; i < b; i++)
+# define printArray(array) for(auto edge: array) {cout << edge << " ";}
 # define sort(arr) sort(arr.begin(), arr.end())
-typedef long long ll;
+# define vi vector<int>
+# define int long long
+# define pb push_back
+# define pii pair<int, int>
+# define seev(v, n) for(int i = 0; i < n; i++) {cin >> x; v.push_back(x);}
+# define sees(s, n) for(int i= 0; i < n; i++) {cin >> x; s.insert(x);}
 const int mod =  1e9 + 7;
+
+int dp[1000005];
+
 using namespace std;
 
-int max(int a, int b) { return (a > b) ? a : b; }
+int max(int a, int b) { return (a > b) ? a : b;}
 
-int main()
+void solve()
+{
+    int n, m, x, i, j, k;
+    
+}
+
+signed main()
 {
     fast_io;
-    ll t, n, m, x, i, j, k;
+    int t;
+
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
     // cin >> t;
     // t = 1;
@@ -23,8 +43,13 @@ int main()
     while (t--)
     {
         // code here
-        
+        solve()
     }
+
+    #ifdef LOCAL
+    clock_t tStart = clock();
+    cerr << fixed<< setprecision(10) << "\nTime Taken: " << (double)(clock()- tStart)/CLOCKS_PER_SEC << endl;
+    #endif
 
     return 0;
 }
